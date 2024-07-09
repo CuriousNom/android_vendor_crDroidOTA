eabaa93c2b6900146d7b2274e45a92f0ae71314f
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2024-07-09 13:30:18 +0530

    Create gta4l_changelog.mk

---
+++ gta4l_changelog.mk
@@ -0,0 +1,8 @@
+Highlights & Device Specific Changes:
+Build type: Monthly
+Device: Samsung Galaxy Tab A7 2020 (gta4l)
+Device maintainer: CuriousNom
+
+===== 08 July, 2024 =====
+
+- Initial crDroid 10.X build for Samsung Galaxy Tab A7 2020
